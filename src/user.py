3aa1b635ca6da8bd3e91f8233224d0dea89bad33
mathias <mathias.ressort@epitech.eu>
2020-11-11 17:09:30 +0100

    [ADD] IA control

--- src/user.py
+++ src/user.py
@@ -1,81 +1,107 @@
-size_window_x = 760
-size_window_y = 840
-ia_bar_pos = 15
-ball_pos = [0, 0]
-ball_speed = [0, 0]
+size_window = [760, 840]
+tile_map = None
+player_pos = [0, 0]
+block_size = 40
+
+ghost_pos_red = [0, 0]
+ghost_pos_pink = [0, 0]
+ghost_pos_blue = [0, 0]
+ghost_pos_orange = [0, 0]
 
 class user():
     def __init__(self):
         return
 
-    def get_size_screen_x(self):
-        global size_window_x
-        return size_window_x
-
-    def get_size_screen_y(self):
-        global size_window_y
-        return size_window_y
+    def get_size_screen(self):
+        global size_window
+        return size_window
 
     def get_pos_player(self):
-        global ball_pos
-        return ball_pos
+        global player_pos
+        return player_pos
 
     def get_map(self):
-        global ball_pos
-        return ball_pos
+        global tile_map
+        return tile_map
 
     def move_ghost_up(self, ghost_type):
         if (ghost_type == "red"):
-            return
+            global ghost_pos_red
+            ghost_pos_red[1] += 40
         elif (ghost_type == "pink"):
-            return
+            global ghost_pos_pink
+            ghost_pos_pink[1] += 40
         elif (ghost_type == "blue"):
-            return
+            global ghost_pos_blue
+            ghost_pos_blue[1] += 40
         else:
-            return
+            global ghost_pos_orange
+            ghost_pos_orange[1] += 40
 
     def move_ghost_down(self, ghost_type):
         if (ghost_type == "red"):
-            return
+            global ghost_pos_red
+            ghost_pos_red[1] -= 40
         elif (ghost_type == "pink"):
-            return
+            global ghost_pos_pink
+            ghost_pos_pink[1] -= 40
         elif (ghost_type == "blue"):
-            return
+            global ghost_pos_blue
+            ghost_pos_blue[1] -= 40
         else:
-            return
+            global ghost_pos_orange
+            ghost_pos_orange[1] -= 40
 
     def move_ghost_left(self, ghost_type):
         if (ghost_type == "red"):
-            return
+            global ghost_pos_red
+            ghost_pos_red[0] -= 40
         elif (ghost_type == "pink"):
-            return
+            global ghost_pos_pink
+            ghost_pos_pink[0] -= 40
         elif (ghost_type == "blue"):
-            return
+            global ghost_pos_blue
+            ghost_pos_blue[0] -= 40
         else:
-            return
+            global ghost_pos_orange
+            ghost_pos_orange[0] -= 40
 
     def move_ghost_right(self, ghost_type):
         if (ghost_type == "red"):
-            return
+            global ghost_pos_red
+            ghost_pos_red[0] += 40
         elif (ghost_type == "pink"):
-            return
+            global ghost_pos_pink
+            ghost_pos_pink[0] += 40
         elif (ghost_type == "blue"):
-            return
+            global ghost_pos_blue
+            ghost_pos_blue[0] += 40
         else:
-            return
+            global ghost_pos_orange
+            ghost_pos_orange[0] += 40
 
 class program_init_user():
     def __init__(self):
         return
 
-    def get_ia_bar_pos(self):
-        global ia_bar_pos
-        return ia_bar_pos
+    def get_ia_ghost_pos(self, ghost_type):
+        if (ghost_type == "red"):
+            global ghost_pos_red
+            return ghost_pos_red
+        elif (ghost_type == "pink"):
+            global ghost_pos_pink
+            return ghost_pos_pink
+        elif (ghost_type == "blue"):
+            global ghost_pos_blue
+            return ghost_pos_blue
+        else:
+            global ghost_pos_orange
+            return ghost_pos_orange
 
-    def set_ball_pos(self, pos):
-        global ball_pos
-        ball_pos = pos
+    def set_map(self, map_1):
+        global tile_map
+        tile_map = map_1
 
-    def set_ball_speed(self, pos):
-        global ball_speed
-        ball_speed = pos
+    def set_player_pos(self, pos):
+        global player_pos
+        player_pos = pos
