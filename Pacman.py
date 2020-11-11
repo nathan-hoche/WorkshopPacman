336da94ef39ca550e4bcf04e6b50cf16ea4f6d1c
mathias <mathias.ressort@epitech.eu>
2020-11-11 17:38:21 +0100

    [UPDATE] pygame to tkinter

--- Pacman.py
+++ Pacman.py
@@ -1,14 +1,7 @@
-import pygame
-
-from pygame.locals import (
-    K_UP,
-    K_DOWN,
-    K_LEFT,
-    K_RIGHT,
-    K_ESCAPE,
-    KEYDOWN,
-    QUIT,
-)
+import tkinter as tk
+from tkinter import Canvas
+import os
+from src.user import program_init_user
 
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
@@ -18,24 +11,22 @@ BLUE = (0, 0, 255)
 size_window_x = 760
 size_window_y = 840
 
-running = True
 fd = open("maps/map_1.txt", "r")
 tile_map = fd.read()
 
-pygame.init()
-screen = pygame.display.set_mode([size_window_x, size_window_y])
-sprite_sheet = pygame.image.load("images/pacman.png").convert()
+IA_params = program_init_user()
+IA_params.set_map(tile_map)
 
 class mapping():
     def _init_(self):
         return
 
-    def draw_map(self, screen):
+    def draw_map(self, canvas):
         posx = 0
         posy = 0
         for block in tile_map:
             if (block == '1'):
-                pygame.draw.rect(screen, BLACK, (posx, posy, 40, 40))
+                canvas.create_rectangle(posx, posy, posx + 40, posy + 40, fill='black')
             posx += 40
             if (posx >= 800):
                 posy += 40
@@ -43,12 +34,10 @@ class mapping():
 
 class Player():
     def __init__(self):
-        self.sprit = pygame.sprite.Sprite()
         self.posx = 7
         self.posy = 7
         self.change_pos = 20
-        self.rect = sprite_sheet.get_rect()
-        #slef.rect. self.posx, self.posy, 13, 13)
+        self.rect = [self.posx, self.posy, 13, 13]
 
     def animation(self):
         global sprite_sheet
@@ -75,42 +64,43 @@ class Ghost():
 
     def init_ghost(self, color):
         if (color == "red"):
-            self.rect = pygame.rect(self.posx, self.red_posy, 14, 14)
+            self.rect = [self.posx, self.red_posy, 14, 14]
         elif (color == "pink"):
-            self.rect = pygame.rect(self.posx, self.pink_posy, 14, 14)
+            self.rect = [self.posx, self.pink_posy, 14, 14]
         elif (color == "blue"):
-            self.rect = pygame.rect(self.posx, self.blue_posy, 14, 14)
+            self.rect = [self.posx, self.blue_posy, 14, 14]
         else:
-            self.rect = pygame.rect(self.posx, self.orange_posy, 14, 14)
+            self.rect = [self.posx, self.orange_posy, 14, 14]
 
-#def set_sound():
-    #pygame.mixer.music.load("music/willy-la-chanson-de-pac-man-audio.mp3")
-    #pygame.mixer.music.play(loops = -1)
-    #win_sound = pygame.mixer.Sound("music/win.ogg")
-    #lose_sound = pygame.mixer.Sound("music/lose.ogg")
+gui = tk.Tk()
+gui.geometry(str(size_window_x) + "x" + str(size_window_y))
+gui.resizable(width = False, height = False)
+gui.title("Workshop Pacman")
+gui['background'] = "#FFFFFF"
 
+if "nt" == os.name:
+    gui.wm_iconbitmap(bitmap = "@images/logo.ico")
+else:
+    gui.wm_iconbitmap(bitmap = "@images/logo.xbm")
+
+#sprite_sheet = tk.PhotoImage(file = "images/pacman.png")
+
+canvas = Canvas(gui, width = size_window_x, height = size_window_y, bd = 0, bg = "white")
+canvas.pack(padx = 0, pady = 0)
+canvas.focus_set()
 
 obj_map = mapping()
+obj_map.draw_map(canvas)
+
 player = Player()
-Ghost = Ghost()
-#set_sound()
+Red_Ghost = Ghost()
+Pink_Ghost = Ghost()
+Blue_Ghost = Ghost()
+Orange_Ghost = Ghost()
 
-while running:
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                running = False
+def game_loop():
 
-        elif event.type == QUIT:
-            running = False
+    gui.after(75, game_loop)
 
-    screen.fill((255, 255, 255))
-    
-    obj_map.draw_map(screen)
-
-    #all_sprites = pygame.sprite.Group()
-    #all_sprites.add(player)
-    
-    pygame.display.flip()
-
-pygame.quit()
+gui.after_idle(game_loop)
+gui.mainloop()
